3ed1607344ae0cec4eea4c47f98605b4e26b5785
John Pavlik <john.a.pavlik@gmail.com>
2020-06-30 13:45:26 -0500

    Update run_tests.ps1

--- run_tests.ps1
+++ run_tests.ps1
@@ -1,15 +1,15 @@
 #1 for vertex packing risk minimize
 #2 for risk constrained vertex packing
-$model_types = 1
+$model_types = 2
 
-$myLog = $PSScriptRoot + '/logs/' + $model_type + '/'
+$myLog = $PSScriptRoot + '/logs/' + $model_type.ToString() + '/'
 $input = $PSScriptRoot + '/graphs/Delta/'
 
 $stdErrLog = $myLog + 'stderr.log'
 $stdOutLog = $myLog + 'stdout.log'
 
-$values = (20, 40, 80, 150, 250)
-#$values = 100
+#$values = (20, 40, 80, 150, 250)
+$values = (0, 100, 1000, 10000)
 $files = Get-ChildItem -Path $input -Force -Recurse
 foreach($model_type in $model_types)
 {
@@ -22,7 +22,7 @@ foreach($model_type in $model_types)
             $error_file = $myLog + $input_file.Name + ".err"
             $input_file_name = -join ("`"", $input, $input_file.Name, "`"")
             Write-Output $output_file
-            Start-Process -File x64\Release\AirplaneSeatAssignment.exe -ArgumentList '-n', $val, '-m', $model_type, $input_file_name  -RedirectStandardOutput $output_file -RedirectStandardError $stdErrLog -wait
+            Start-Process -FilePath "${PSScriptRoot}/x64/Release/AirplaneSeatAssignment.exe" -ArgumentList '-n', $val, '-m', $model_type, $input_file_name  -RedirectStandardOutput $output_file -RedirectStandardError $stdErrLog -wait
         }
     }
 }
